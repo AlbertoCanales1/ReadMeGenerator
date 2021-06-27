bf8c70ec349ff6463c6e4b900cf6ae152921a38c
AlbertoCanales1 <albertocanales1@outlook.com>
2021-06-26 20:22:15 -0500

    Created prompt questions

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was your motivation?',
+    },
+    {
+        type: 'input',
+        name: 'Why',
+        message: 'Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")',
+    },
+    {
+        type: 'input',
+        name: 'problems',
+        message: 'What problems did you solve?',
+    },
+    {
+        type: 'input',
+        name: 'learn',
+        message: 'What did you learn?',
+    },
+    {
+        type: 'input',
+        name: 'stand_out',
+        message: 'What makes your project stand out?'
+    }
+
+    
+])
